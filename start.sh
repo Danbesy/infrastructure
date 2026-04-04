8c993e1172f30b37cb5c6f7bd687ae8f050b550b
Danbesy <danya.danbesy@gmail.com>
2026-04-04 16:05:37 +0000

    initial commit

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Остановка всех сервисов..."
+docker compose -f monitoring/docker-compose.yaml down
+docker compose -f mtproto-proxy/docker-compose.yaml down
+docker compose -f mail/docker-compose.yaml down
+docker compose -f reverse-proxy/docker-compose.yaml down
+docker compose -f infrastructure.yaml down
+
+echo "Очистка неиспользуемых сетей Docker..."
+docker network prune -f
+
+echo "Запуск всех сервисов..."
+docker compose -f infrastructure.yaml up -d --remove-orphans
+docker compose -f monitoring/docker-compose.yaml up -d --force-recreate
+docker compose -f mtproto-proxy/docker-compose.yaml up -d --force-recreate
+docker compose -f mail/docker-compose.yaml up -d --force-recreate
+docker compose -f reverse-proxy/docker-compose.yaml up -d --force-recreate
