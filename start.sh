3832780d85d6bc6a04825d15bf395d65124c01ad
Danbesy <danya.danbesy@gmail.com>
2026-04-06 18:23:59 +0000

    chore: update start script

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ echo "Остановка всех сервисов..."
 docker compose -f monitoring/docker-compose.yaml down
 docker compose -f mtproto-proxy/docker-compose.yaml down
 docker compose -f mail/docker-compose.yaml down
+docker compose -f matrix/docker-compose.yaml down
 docker compose -f reverse-proxy/docker-compose.yaml down
 docker compose -f infrastructure.yaml down
 
@@ -14,4 +15,5 @@ docker compose -f infrastructure.yaml up -d --remove-orphans
 docker compose -f monitoring/docker-compose.yaml up -d --force-recreate
 docker compose -f mtproto-proxy/docker-compose.yaml up -d --force-recreate
 docker compose -f mail/docker-compose.yaml up -d --force-recreate
+docker compose -f matrix/docker-compose.yaml up -d --force-recreate
 docker compose -f reverse-proxy/docker-compose.yaml up -d --force-recreate
